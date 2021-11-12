e1554ac1eb0590a3ac713a0488c64354023134a6
Riski473 <86213839+Riski473@users.noreply.github.com>
2021-11-12 11:19:10 +0700

    Update tew.sh

--- tew.sh
+++ tew.sh
@@ -1,4 +1,4 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz
 tar -xf lolMiner_v1.32a_Lin64.tar.gz
 cd 1.32a
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TKMzEThte1Nji2rKMHnK5Ggb46UMi3YVmT.6#df70-vshw --ethstratum ETHPROXY
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user RVN:rLYMxwxrVuQMsGeZ9Vw1xbuosMo5MKzovh.5#z7nb-jklb --ethstratum ETHPROXY
